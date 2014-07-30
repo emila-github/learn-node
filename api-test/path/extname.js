822ba5dba78fe7d5966988062bde69d8713e243f
bsky1wcj <bsky1wcj@163.com>
2014-07-30 15:43:51 +0800

    path
    sep; extname;

---
+++ api-test/path/extname.js
@@ -0,0 +1,3 @@
+var path = require('path');
+//返回文件扩展名
+console.log(path.extname('file.png'));     //.png
---
+++ api-test/path/sep.js
@@ -0,0 +1,3 @@
+var path = require('path');
+//返回路径分隔符   window环境 返回 \ ; 其他系统返回 /;
+console.log(path.sep);
